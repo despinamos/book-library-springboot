e3fb41544dd7847d18b15440067ab2bc7d22afa6
D. Moschokarfi <despinamx16@gmail.com>
2026-09-10 14:41:18 +0300

    feat: used mapper for methods

--- src/main/java/com/demo/library_app/service/BookService.java
+++ src/main/java/com/demo/library_app/service/BookService.java
@@ -5,46 +5,31 @@ import com.demo.library_app.core.exception.EntityNotFoundException;
 import com.demo.library_app.dto.BookInsertDTO;
 import com.demo.library_app.dto.BookPatchDTO;
 import com.demo.library_app.dto.BookReadOnlyDTO;
+import com.demo.library_app.mapper.Mapper;
 import com.demo.library_app.model.Book;
 import com.demo.library_app.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final Mapper mapper;
 
     public List<BookReadOnlyDTO> getAllBooks() {
         return bookRepository.findAll()
                 .stream()
-                .map(book -> new BookReadOnlyDTO(
-                        book.getId(),
-                        book.getTitle(),
-                        book.getAuthor(),
-                        book.getIsbn(),
-                        book.getGenre(),
-                        book.getPublishedYear(),
-                        book.isAvailable()
-                ))
+                .map(mapper::mapToBookReadOnlyDTO)
                 .toList();
     }
 
     public BookReadOnlyDTO getBookByID(Long id) {
         Book book = bookRepository.getReferenceById(id);
-        return new BookReadOnlyDTO(
-                book.getId(),
-                book.getTitle(),
-                book.getAuthor(),
-                book.getIsbn(),
-                book.getGenre(),
-                book.getPublishedYear(),
-                book.isAvailable()
-        );
+        return mapper.mapToBookReadOnlyDTO(book);
     }
 
     public BookReadOnlyDTO createBook(BookInsertDTO dto) {
@@ -56,26 +41,11 @@ public class BookService {
             );
         }
 
-        Book book = new Book();
-
-        book.setTitle(dto.getTitle());
-        book.setAuthor(dto.getAuthor());
-        book.setIsbn(dto.getIsbn());
-        book.setGenre(dto.getGenre());
-        book.setPublishedYear(dto.getPublishedYear());
-        book.setAvailable(dto.getAvailable());
+        Book book = mapper.mapToBookEntity(dto);
 
         Book savedBook = bookRepository.save(book);
 
-        return new BookReadOnlyDTO(
-                savedBook.getId(),
-                savedBook.getTitle(),
-                savedBook.getAuthor(),
-                savedBook.getIsbn(),
-                savedBook.getGenre(),
-                savedBook.getPublishedYear(),
-                savedBook.isAvailable()
-        );
+        return mapper.mapToBookReadOnlyDTO(savedBook);
     }
 
     public BookReadOnlyDTO patchBookById(Long id, BookPatchDTO dto){
@@ -108,15 +78,7 @@ public class BookService {
 
         Book updatedBook = bookRepository.save(book);
 
-        return new BookReadOnlyDTO(
-                updatedBook.getId(),
-                updatedBook.getTitle(),
-                updatedBook.getAuthor(),
-                updatedBook.getIsbn(),
-                updatedBook.getGenre(),
-                updatedBook.getPublishedYear(),
-                updatedBook.isAvailable()
-        );
+        return mapper.mapToBookReadOnlyDTO(updatedBook);
     }
 
     public void deleteBookById(Long id) {
